501d3f6ba837fe5f659048f4b7baac83a8662fe8
rolypolyvg295 <22863750+rolypolyvg295@users.noreply.github.com>
2022-08-01 12:44:01 +0900

    Add files via upload

---
+++ db/init/create_basic.sql
@@ -0,0 +1,30 @@
+SET GLOBAL local_infile=1; /* For loading local files  */
+
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '';
+
+USE records;
+
+CREATE TABLE trace 
+    (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(100),
+        UNIQUE (name)
+    );
+
+CREATE TABLE tx
+    (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        traceId INT,
+        requestsId TEXT, 
+        gen INT NOT NULL,
+        fin INT NOT NULL,
+        txSource TEXT NOT NULL, 
+        txType TEXT NOT NULL,
+        txId INT,
+
+        FOREIGN KEY (traceID)
+            REFERENCES trace(id)
+            ON DELETE CASCADE
+    );
+
+    
---
+++ server/app/main.js
@@ -0,0 +1,194 @@
+const express = require('express')
+const mysql = require('mysql2/promise')
+// const csv = require('fast-csv')
+
+async function createConn() {
+    return mysql.createConnection({
+        host: 'db',
+        user: 'root',
+        password: '',
+        database: 'records'
+    })
+}
+
+
+// connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+//   if (err) throw err
+
+//   console.log('The solution is: ', rows[0].solution)
+// })
+
+// connection.end()
+
+
+const app = express()
+
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true,
+    limit: '100mb'
+}))
+
+const port = 3000
+
+console.log(`Starting server at port: ${port}`)
+
+app.get('/', (req, res) => {
+    res.send('HW')
+})
+
+app.get('/tx/:traceId', async (req, res) => {
+    let traceId = req.params.traceId;
+    let sqlStmt = req.query.sql;
+    let traceIdStmt = sqlStmt.replace(/\btx\b/ig, `tx_${traceId}`)
+
+    console.log(`Querying: ${traceIdStmt}`)
+    var connection;
+    try {
+        connection = await createConn()
+
+        let result = await connection.query(traceIdStmt)
+        connection.end()
+
+        console.log(`TX Query Result: ${result[0].length} rows`)
+        return res.send(result[0])
+    } catch (e) {
+        let clientMsg = 'sqlMessage' in e ? e.sqlMessage : 'Something went wrong'
+        console.log('cm', clientMsg)
+        return handleErrorMsg(res, e, 500, clientMsg,
+            `Some problem with db: /tx/${traceId}?sql=${sqlStmt}`)
+    }
+})
+
+function handleErrorMsg(res, e, statusCode, clientMsg, logMsg) {
+    console.log(logMsg)
+    if (e != null) {
+        console.log(e)
+    }
+    return res.status(statusCode).send(clientMsg)
+}
+
+app.get('/trace', async (req, res) => {
+    let sqlStmt = 'SELECT * FROM trace order by id';
+    var connection;
+    try {
+        connection = await createConn()
+
+        let result = await connection.query(sqlStmt)
+        connection.end()
+
+        console.log(`Trace Query Result: ${result[0].length} rows`)
+        return res.send(result[0])
+    } catch (e) {
+        return handleErrorMsg(res, e, 500, 'Something wrong with db.',
+            `Some problem with db: /trace`)
+    }
+})
+
+app.delete('/trace/:traceId', async (req, res)=> {
+    let traceId = req.params.traceId
+
+    let delStmt = `DELETE FROM trace WHERE id = ${traceId}`;
+    var connection;
+    try {
+        connection = await createConn()
+
+        let result = await connection.query(delStmt)
+        connection.end()
+
+        let deleted = result[0].affectedRows == 1;
+
+        var msg;
+        if (deleted) {
+            msg = `Deleted trace id: ${traceId}`
+        } else {
+            msg = `Trace with ${traceId} not found.`
+        }
+        console.log(msg)
+        return res.send(msg)
+    } catch (e) {
+        return handleErrorMsg(res, e, 500, 'Something wrong with db.',
+            `Some problem with db: delete /trace/${traceId}`)
+    }
+}) 
+
+app.post('/trace/:traceName?', async (req, res)=> {
+    let traceName = req.params.traceName ? req.params.traceName : new Date().toLocaleString();
+    var connection;
+    try {
+        connection = await createConn()
+
+        await connection.beginTransaction();
+
+        let checkTrace = 'SELECT * FROM trace WHERE name = ?'
+        let checked = await connection.query(checkTrace, [traceName])
+        if (checked[0].length >= 1) {
+            if (req.params.traceName) {
+                await connection.rollback()
+                return handleErrorMsg(res, null, 422, 'Trace name must be unique.',
+                    `Duplicate trace name received: ${traceName}`)
+            } else {
+                let cnt = 1;
+                while (true) {
+                    let newName = `${traceName}_${cnt}`
+                    let checked2 = await connection.query(checkTrace, [newName])
+                    if (checked2[0].length == 0) {
+                        traceName = newName;
+                        break;
+                    } else {
+                        cnt += 1
+                    }
+                }
+            }
+        }
+
+
+        let traceSql = 'INSERT INTO trace (name) VALUES (?)'
+        let traceResult = await connection.query(traceSql, [traceName])
+        let traceId = traceResult[0].insertId
+        console.log(`New trace inserted: ${traceName}`)
+
+
+        let csv = Object.keys(req.body)[0].trim();
+        let lines = csv.split(/\n|\r\n/)
+        let rows = lines.map(function (v) {
+            let line = v.split(',');
+            var gen = parseInt(line[0]),
+                fin = parseInt(line[1]),
+                requestsId = line[2],
+                txSource,
+                txType,
+                txId;
+        
+            if (line.length === 4) {
+                txSource = line[2];
+                txType = line[3];
+                txId = null;
+            } else {
+                txSource = line[3];
+                txType = line[4];
+                txId = parseInt(line[5]);
+            }
+
+            return [traceId, gen, fin, requestsId, txSource, txType, txId]
+        })
+    
+        let txSql = 'INSERT INTO tx (traceId, gen , fin, requestsId, txSource, txType, txId) VALUES ?'
+        let result = await connection.query(txSql, [rows])
+
+        let viewSql = `CREATE VIEW tx_${traceId} AS SELECT * FROM tx WHERE traceId=${traceId}`
+        await connection.query(viewSql)
+
+        await connection.commit()
+
+        console.log(`Inserted ${result[0].affectedRows} txs to trace: ${traceName}`)
+        return res.send(`Inserted ${result[0].affectedRows} txs to trace: ${traceName}`)
+    } catch (e) {
+        await connection.rollback()
+        return handleErrorMsg(res, e, 500, 'Something wrong with db.',
+            `Some problem with db: /trace/${traceName}`)
+    }
+})
+
+app.listen(port);
